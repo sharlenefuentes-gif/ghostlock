e9874292dac04c5cd35fe2843617bbc287b4e856
sharlenefuentes-gif <sharlene.fuentes@evsu.edu.ph>
2025-12-15 21:18:38 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'magic-lock-v1';
+const ASSETS = [
+  './', // Caches the root page
+  './index.html',
+  './styles.css',
+  './script.js',
+  './manifest.json'
+];
+
+// Install the Service Worker and Cache all core files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Serve Cached Files when Offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
