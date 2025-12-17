574b58e8440d39ed98dec65c5c34285135468a8c
sharlenefuentes-gif <sharlene.fuentes@evsu.edu.ph>
2025-12-17 12:41:23 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,13 @@
-const CACHE_NAME = 'magic-lock-v1';
+const CACHE_NAME = 'ghost-lock-v1';
 const ASSETS = [
-  './', // Caches the root page
+  './', 
   './index.html',
   './styles.css',
   './script.js',
   './manifest.json',
-  './icon.png' // Ensure the icon is also cached for offline
+  './icon.jpg' 
 ];
 
-// Install the Service Worker and Cache all core files
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -17,7 +16,6 @@ self.addEventListener('install', (e) => {
   );
 });
 
-// Serve Cached Files when Offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
